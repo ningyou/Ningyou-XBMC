42afb6a094958262f3c81c8d72b47990391a3aaf
Stein-Ivar Berghei <stein-ivar@berghei.no>
2013-03-22 18:53:51 +0100

    Use websockets api

--- service.ningyou/default.py
+++ service.ningyou/default.py
@@ -3,4 +3,19 @@ import xbmc, xbmcgui, xbmcaddon, os, sys
 from ningyou import Ningyou
 
 xbmc.log("Ningyou updater is starting...")
-Ningyou().run()
+ws = Ningyou('ws://localhost:9090/jsonrpc')
+
+while(not (xbmc.abortRequested or ws.abort)):
+	try:
+		ws.connect()
+		ws._th.join()
+	except:
+		xbmc.log("Ningyou: failed to connect")
+		continue
+
+try:
+	print "Closing"
+	ws.close()
+except:
+	xbmc.log("Ningyou: failed to close websocket")
+	raise
--- service.ningyou/ningyou.py
+++ service.ningyou/ningyou.py
@@ -1,28 +1,38 @@
 import xbmc, xbmcaddon, xbmcvfs, os, sys, time
 import telnetlib
 import urllib, urllib2
-import threading
 
 try: import simplejson as json
 except ImportError: import json
 
-class Ningyou(threading.Thread):
-	addon_id = "service.ningyou"
-	Addon = xbmcaddon.Addon(addon_id)
-	datadir = Addon.getAddonInfo('profile')
-	addondir = Addon.getAddonInfo('path')
-	url = "http://ningyou-project.org/api"
+addon_id = "service.ningyou"
+Addon = xbmcaddon.Addon(addon_id)
+datadir = Addon.getAddonInfo('profile')
+addondir = Addon.getAddonInfo('path')
+url = "http://ningyou-project.org/api"
+
+sys.path.append(xbmc.translatePath(os.path.join(addondir, 'resources', 'lib')))
+
+from threadedclient import WebSocketClient
+
+class Ningyou(WebSocketClient):
 	abort = False
+
+	def opened(self):
+		xbmc.log("Ningyou: connected via websockets")
+
+	def closed(self):
+		xbmc.log("Ningyou: closed websocket")
 
 	def API(self, method, params = []):
 		data = {
 			"version": 1,
 			"method": method,
 			"params": params,
-			"token": self.Addon.getSetting("ningyou_token"),
+			"token": Addon.getSetting("ningyou_token"),
 		}
 		data = urllib.urlencode(data, doseq=True)
-		req = urllib2.Request(self.url, data)
+		req = urllib2.Request(url, data)
 		response = urllib2.urlopen(req)
 		result = response.read()
 
@@ -34,21 +44,29 @@ class Ningyou(threading.Thread):
 		return result
 
 	def findInList(self, id):
-		lists = self.API('getlists')
+		if not id:
+			return xbmc.log("Ningyou: ID was not passed to findInList")
+
+		try:
+			lists = self.API('getlists')
+		except:
+			xbmc.log("Unable to parse api response")
+			return
+
 		if lists:
 			for list in lists:
-				playlist = "special://videoplaylists/%s.xsp" % list['name']
+				playlist = "special://profile/playlists/video/%s.xsp" % list['name']
 				if xbmcvfs.exists(playlist):
 					query = xbmc.executeJSONRPC('{"jsonrpc":"2.0","method":"Files.GetDirectory","params":{"directory":"%s"},"id":1}' % playlist)
 					query = unicode(query, 'utf-8', errors='ignore')
 					response = json.loads(query)
 					files = response['result']['files']
-
 					if id in (obj['id'] for obj in files):
 						return list['name']
 
-	def handleMessage(self, message):
-		data = json.loads(message)
+	def received_message(self, message):
+		data = json.loads(unicode(message))
+
 		if 'method' in data and 'params' in data and 'sender' in data['params'] and data['params']['sender'] == 'xbmc':
 			if data['method'] == 'VideoLibrary.OnUpdate' and data['params']['data']['item']['type'] == 'episode':
 				episodeid = data['params']['data']['item']['id']
@@ -56,7 +74,6 @@ class Ningyou(threading.Thread):
 				query = unicode(query, 'utf-8', errors='ignore')
 				response = json.loads(query)
 				info = response['result']['episodedetails']
-
 				query = xbmc.executeJSONRPC('{"jsonrpc":"2.0","method":"VideoLibrary.GetTVShowDetails","params":{"properties":["imdbnumber"],"tvshowid":%d}, "id":1}' % info['tvshowid'])
 				query = unicode(query, 'utf-8', errors='ignore')
 				response = json.loads(query)
@@ -83,52 +100,3 @@ class Ningyou(threading.Thread):
 
 			elif data['method'] == 'System.OnQuit':
 				self.abort = True
-
-	def run(self):
-		while(not (xbmc.abortRequested or self.abort)):
-			time.sleep(1)
-			try:
-				tn = telnetlib.Telnet('localhost', 9090, 10)
-			except IOError as (errno, strerror):
-				#connection failed, try again soon
-				xbmc.log("Ningyou: Telnet too soon? ("+str(errno)+") "+strerror)
-				time.sleep(1)
-				continue
-
-			bCount = 0
-			xbmc.log("Ningyou: connected.")
-
-			while(not (xbmc.abortRequested or self.abort)):
-				try:
-					if bCount == 0:
-						notification = ""
-						inString = False
-					[index, match, raw] = tn.expect(["(\\\\)|(\\\")|[{\"}]"], 0.2) #note, pre-compiled regex might be faster here
-					notification += raw
-					if index == -1: # Timeout
-						continue
-					if index == 0: # Found escaped quote
-						match = match.group(0)
-						if match == "\"":
-							inString = not inString
-							continue
-						if match == "{":
-							bCount += 1
-						if match == "}":
-							bCount -= 1
-					if bCount > 0:
-						continue
-					if bCount < 0:
-						bCount = 0
-				except EOFError:
-					break #go out to the other loop to restart the connection
-
-				self.handleMessage(notification)
-
-		try:
-			tn.close()
-		except:
-			xbmc.log("Ningyou: Encountered error attempting to close the telnet connection")
-			raise
-		xbmc.log('Ningyou: exiting')
-		sys.exit(0)
